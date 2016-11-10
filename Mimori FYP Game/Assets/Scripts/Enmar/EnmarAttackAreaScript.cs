75f3224f6a4ff023510f7add6742450932d27c35
Auraby <tan_alwin97@yahoo.com.sg>
2016-11-10 16:32:40 +0800

    Enmar Updates
    -Finished Enmar's FSM
    -Added Enmar Laser Controller
    -Added Enmar placeholder animations
    -Fixing Enmar's animations

--- Mimori FYP Game/Assets/Scripts/Enmar/EnmarAttackAreaScript.cs
+++ Mimori FYP Game/Assets/Scripts/Enmar/EnmarAttackAreaScript.cs
@@ -7,14 +7,12 @@ public class EnmarAttackAreaScript : MonoBehaviour {
 
     public areaName aName;
 
-    ParticleSystem part;
-
     public static EnmarAttackAreaScript instance { get; set; }
 
     // Use this for initialization
     void Start () {
         instance = this;
-        part = gameObject.GetComponent<ParticleSystem>();
+       
 	}
 	
 	// Update is called once per frame
@@ -63,20 +61,5 @@ public class EnmarAttackAreaScript : MonoBehaviour {
         }
     }
 
-    public void OnParticleCollision(GameObject other)
-    {
-       // Debug.Log("Hit player");
-
-        if(other.gameObject.tag == "Player")
-        {
-            var ps = part.subEmitters;
-            ps.collision1.maxParticles = 0;
-            Debug.Log("Hit Player");
-        }
-    }
-
-    public void OnParticleTrigger(GameObject other)
-    {
-
-    }
+   
 }
