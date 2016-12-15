8294a7d3720a203f75288b4131ecd81e7d6329b1
Chen Ming <chenming_97@hotmail.com>
2016-12-15 18:27:04 +0800

    CM Updates (15th Dec 2016)
    - Forest of Mimory design

--- Mimori FYP Game/Assets/Scripts/GameController.cs
+++ Mimori FYP Game/Assets/Scripts/GameController.cs
@@ -11,8 +11,7 @@ public class GameController : MonoBehaviour
     public string currentScene;
     public static bool loadingGame = false;
     public static GameController gameController;
-    //Async
-    AsyncOperation asyncOp; 
+    
     //Player Position
     public float playerPositionX;
     public float playerPositionY;
@@ -23,21 +22,6 @@ public class GameController : MonoBehaviour
     public bool outpost3Captured = false;
     public bool outpost4Captured = false;
     public bool houseTrapActivated = false;
-
-    #region Game Over Variables
-    [Header("GameOverScreen Variables")]
-    public GameObject gameOverScreen;
-    public Image gameoverBlackPanel;
-    public Text gameoverText;
-    public Text gameoverTextSubtitle;
-
-    public float gameOverWaitTime = 6f;
-    public float gameoverTime = 0f;
-
-    //Make this true if game over from anything
-    public bool loseGame = false;
-    public bool playerDie = false;
-    #endregion
 
     // Use this for initialization
     void Awake()
@@ -55,13 +39,7 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        //Async
-        asyncOp = SceneManager.LoadSceneAsync("MainMenu");
-        asyncOp.allowSceneActivation = false;
-        //GameOver Screen
-        gameoverBlackPanel.canvasRenderer.SetAlpha(0.0f);
-        gameoverText.canvasRenderer.SetAlpha(0.0f);
-        gameoverTextSubtitle.canvasRenderer.SetAlpha(0.0f);
+        
     }
 
     void Update()
@@ -70,57 +48,6 @@ public class GameController : MonoBehaviour
         {
             currentScene = SceneManager.GetActiveScene().name;
         }
-
-        #region Lose Conditions
-        //Lose Conditions here
-        if (Health.instance.currentHealth <= 0)
-        {
-            loseGame = true;
-            playerDie = true;
-        }
-
-        if (SceneManager.GetActiveScene().name == "Gate Of Telluris") {
-            if (Level1Controller.instance.currentWallHealth <= 0 )
-            {
-                loseGame = true;
-            }
-        }
-        
-        #endregion
-
-        #region Gameover screen
-        if (loseGame == true)
-        {
-            gameoverTime += Time.deltaTime;
-
-            gameoverBlackPanel.CrossFadeAlpha(1, 2, false);
-            gameoverText.CrossFadeAlpha(1, 3, false);
-            gameoverTextSubtitle.CrossFadeAlpha(1, 5, false);
-
-            // Change the subtitles according to how the game is lost
-            if (playerDie == true)
-            {
-                gameoverTextSubtitle.text = "You Died";
-            }
-
-            if (SceneManager.GetActiveScene().name == "Gate Of Telluris")
-            {
-                if (Level1Controller.instance.wallDestroyed == true)
-                {
-                    gameoverTextSubtitle.text = "Wall Destroyed";
-                }
-            }
-            
-            if (gameoverTime > gameOverWaitTime)
-            {
-                //load checkpoint here(after a while) (if want do buttons then change the code)
-                asyncOp.allowSceneActivation = true;
-            }
-
-        }
-        #endregion
-
-
         //Debug.Log (currentScene);
     }
 
