200576aa0f89e58dca2d7db31c4b7b361034555d
justinysg <overall_gamer@hotmail.com>
2017-01-26 12:45:31 +0800

    skill tree completed
    Skill tree Final completed

--- Mimori FYP Game/Assets/Scripts/HUD/Slot.cs
+++ Mimori FYP Game/Assets/Scripts/HUD/Slot.cs
@@ -11,16 +11,19 @@ public class Slot : MonoBehaviour , IDropHandler{
 		}
 	}*/
 
-	public  static GameObject Slot1;
-	public static GameObject Slot2;
-	public static GameObject Slot3;
-	public static GameObject Slot4;
+	public   static GameObject Slot1;
+	public  static GameObject Slot2;
+	public  static GameObject Slot3;
+	public  static GameObject Slot4;
 	public  static GameObject[] slotobjectlist = new GameObject[4];
 	public  static bool[] slotobjectisTaken = new bool[4]{false, false, false, false};
-	public static int i;
+	public 	static int[] counterforloop = new int[4];
+	public   int i;
 	private  string getcurrskillname;
-	private  GameObject currskill;
+	public  GameObject currskill;
 	private  GameObject newskill;
+	public GameObject prevskill;
+	public bool checkforloop = false;
 
 	void Start(){
 		i = 0;
@@ -28,12 +31,25 @@ public class Slot : MonoBehaviour , IDropHandler{
 	}
 
 	public void ManageSlots(GameObject newskills){
+
 		if (this.gameObject.name == "Slot 1") {
 			DragHandler.slotchecklist [0] = 1;
 			DragHandler.SlotIsTaken [0] = true;
 			slotobjectisTaken[0] = true;
 			slotobjectlist [0] = DragHandler.itemBeingDragged.gameObject;
-			currskill = newskills;
+			counterforloop [0] = 1;
+
+			if (currskill != null) {
+				prevskill = currskill;
+				currskill = newskills;
+			} else {
+				currskill = newskills;
+			}
+
+			if (prevskill != null) {
+				Destroy (prevskill);
+				prevskill = null;
+			}
 			Debug.Log ("ManageSlots1");
 
 		} else if (this.gameObject.name == "Slot 2") {
@@ -41,49 +57,91 @@ public class Slot : MonoBehaviour , IDropHandler{
 			DragHandler.SlotIsTaken [1] = true;
 			slotobjectisTaken[1] = true;
 			slotobjectlist [1] = DragHandler.itemBeingDragged.gameObject;
-			currskill = newskills;
+			counterforloop [1] = 2;
+
+			if (currskill != null) {
+				prevskill = currskill;
+				currskill = newskills;
+			}
+			else {
+				currskill = newskills;
+			}
+			if (prevskill != null) {
+				Destroy (prevskill);
+				prevskill = null;
+			} 
 			Debug.Log ("ManageSlots2");
 		} else if (this.gameObject.name == "Slot 3") {
 			DragHandler.slotchecklist [2] = 3;
 			DragHandler.SlotIsTaken [2] = true;
 			slotobjectisTaken[2] = true;
 			slotobjectlist [2] = DragHandler.itemBeingDragged.gameObject;
-			currskill = newskills;
+			counterforloop [2] = 3;
+			if (currskill != null) {
+				prevskill = currskill;
+				currskill = newskills;
+			}
+			else {
+				currskill = newskills;
+			}
+			if (prevskill != null) {
+				Destroy (prevskill);
+				prevskill = null;
+			} 
 			Debug.Log ("ManageSlots3");
 		} else {
 			DragHandler.slotchecklist [3] = 4;
 			DragHandler.SlotIsTaken [3] = true;
 			slotobjectisTaken[3] = true;
 			slotobjectlist [3] = DragHandler.itemBeingDragged.gameObject;
-			currskill = newskills;
+			counterforloop [3] = 4;
+			Debug.Log ("ManageSlots4");
+			if (currskill != null) {
+				prevskill = currskill;
+				currskill = newskills;
+			}
+			else {
+				currskill = newskills;
+			}
+			if (prevskill != null) {
+				Destroy (prevskill);
+				prevskill = null;
+			} 
 		}
+
+		/*for (int c = 0; c < slotobjectlist.Length; c++) {
+			if (slotobjectlist [c].gameObject != null) {
+				if (slotobjectlist [c].gameObject.name.Contains (DragHandler.itemBeingDragged.gameObject.name)) {
+					Debug.Log ("GAMEOBJECT DELETED");
+					slotobjectisTaken [c] = false;
+					Destroy (slotobjectlist [c].gameObject);
+					break;
+				}
+			}
+
+		}*/
+		//DeleteSlots ();
 	}
 		
 	public void DeleteSlots(){
-		
-		for (int c = 0; c < 4; c++) {
-			if (slotobjectlist [c].gameObject.name.Contains (DragHandler.itemBeingDragged.gameObject.name)) {
-				Debug.Log ("GAMEOBJECT DELETED");
-				slotobjectisTaken [c] = false;
-				Destroy (slotobjectlist [c].gameObject);
-				break;
-			} else if (slotobjectlist [c].gameObject == null) {
 
+		for (int c = 0; c < slotobjectlist.Length; c++) {
+			if (slotobjectlist [c] == null) {
+				
+			} else {
+				if (slotobjectlist [c].gameObject != null) {
+					if (slotobjectlist [c].gameObject.name.Contains (DragHandler.itemBeingDragged.gameObject.name)) {
+						Debug.Log ("GAMEOBJECT DELETED");
+						slotobjectisTaken [c] = false;
+						Destroy (slotobjectlist [c].gameObject);
+						break;
+					}
+				}
+			 
 			}
 		}
 
-		/*if (this.gameObject == Slot1) {
-			Destroy (currskill);
-		} else if (this.gameObject == Slot2) {
-			Destroy (currskill);
-
-		} else if (this.gameObject == Slot3) {
-			Destroy (currskill);
-		} else if (this.gameObject == Slot4) {
-			Destroy (currskill);
-		}*/
-
-
+		ManageSlots (newskill);
 
 	}
 
@@ -107,29 +165,38 @@ public class Slot : MonoBehaviour , IDropHandler{
 	public void OnDrop (PointerEventData eventData)
 	{
 		newskill = DragHandler.itemBeingDragged.gameObject;
-		for (i = 0; i < DragHandler.slotchecklist.Length; i++) {
-			if (slotobjectisTaken [i] == false) {
-				//DragHandler.slotchecklist [i] = 1;
-				DragHandler.SlotIsTaken [i] = true;
+		for (i = 0; i < 5; i++) {
+				//Debug.Log (counterforloop.GetValue (i));
+			Debug.Log(counterforloop.GetValue(i));
+			if (slotobjectisTaken [i] == false && counterforloop [i] == 0) {
+					//DragHandler.slotchecklist [i] = 1;
+					//DragHandler.SlotIsTaken [i] = true;
+					//slotobjectisTaken [i] = true;
+					Debug.Log (counterforloop.GetValue (i));
+					Debug.Log (i);
+					
+					//DragHandler.getGameObjectSkill [i] = DragHandler.itemBeingDragged.gameObject;
+					//DragHandler.skillName [i] = DragHandler.itemBeingDragged.name;
+					//DragHandler.CurrSlotItem = DragHandler.itemBeingDragged.gameObject;
 
-				//DragHandler.getGameObjectSkill [i] = DragHandler.itemBeingDragged.gameObject;
-				//DragHandler.skillName [i] = DragHandler.itemBeingDragged.name;
-				//DragHandler.CurrSlotItem = DragHandler.itemBeingDragged.gameObject;
-				ManageSlots (newskill);
+					//ManageSlots (newskill);
+				DeleteSlots();
 			
-				DragHandler.itemBeingDragged.transform.position = this.gameObject.transform.position;
-				DragHandler.itemBeingDragged.transform.SetParent(this.gameObject.transform);
-				DragHandler.itemBeingDragged.transform.localScale = new Vector3(DragHandler.itemBeingDragged.transform.localScale.x/1.0f, DragHandler.itemBeingDragged.transform.localScale.y/0.65f, DragHandler.itemBeingDragged.transform.localScale.z/1.0f);
-				DragHandler.itemBeingDragged.GetComponent<DragHandler> ().enabled = false;
-				DragHandler.itemBeingDragged.transform.SetAsFirstSibling ();
-				//Debug.Log (DragHandler.SlotIsTaken [i]);
-				//Debug.Log (i);
-				break;
+					DragHandler.itemBeingDragged.transform.position = this.gameObject.transform.position;
+					DragHandler.itemBeingDragged.transform.SetParent (this.gameObject.transform);
+					DragHandler.itemBeingDragged.transform.localScale = new Vector3 (DragHandler.itemBeingDragged.transform.localScale.x / 1.0f, DragHandler.itemBeingDragged.transform.localScale.y / 0.65f, DragHandler.itemBeingDragged.transform.localScale.z / 1.0f);
+					DragHandler.itemBeingDragged.GetComponent<DragHandler> ().enabled = false;
+					DragHandler.itemBeingDragged.transform.SetAsFirstSibling ();
+					//Debug.Log (DragHandler.SlotIsTaken [i]);
+					//Debug.Log (i);
+					break;
 
-			} else if(slotobjectisTaken [i] == true){
+			} else if(slotobjectisTaken [i] == true && counterforloop [i] == i + 1){
+				
+				
 				 //DragHandler.isDraggedIntoSlot = true;
+					//Debug.Log(slotobjectisTaken [i]);
 					DeleteSlots ();	
-				   ManageSlots (newskill);
 					//DragHandler.getGameObjectSkill [i] = DragHandler.itemBeingDragged.gameObject;
 					//DragHandler.CurrSlotItem = DragHandler.itemBeingDragged.gameObject;
 					//DragHandler.skillName [i] = DragHandler.itemBeingDragged.name;
