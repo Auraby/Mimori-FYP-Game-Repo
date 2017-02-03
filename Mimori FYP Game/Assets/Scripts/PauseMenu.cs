fc1302e301d2a5bd2daaa1bc55d3911f6341d065
Chen Ming <chenming_97@hotmail.com>
2017-02-03 10:49:56 +0800

    CM Update (3rd Feb 2017)
    FOREST BUGGED OMG

---
+++ Mimori FYP Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseMenu;
+
+    bool gamePaused = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log(gamePaused);
+        if (Input.GetKeyDown(KeyCode.O)) {
+            if (!gamePaused) {
+                pauseMenu.gameObject.SetActive(true);
+                gamePaused = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                pauseMenu.gameObject.SetActive(false);
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+	}
+}
