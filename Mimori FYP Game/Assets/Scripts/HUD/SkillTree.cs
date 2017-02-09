55912e8e548c79c544a9b27fd8ef1489e5d39716
justinysg <overall_gamer@hotmail.com>
2017-02-09 20:49:24 +0800

    HUD Major Update V1.0
    New Player Tab
    Mod Skills Abilities
    Player Class
    Abilities Effect
    UI Enlargement

--- Mimori FYP Game/Assets/Scripts/HUD/SkillTree.cs
+++ Mimori FYP Game/Assets/Scripts/HUD/SkillTree.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Linq;
 using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class SkillTree : MonoBehaviour {
 
@@ -19,8 +22,7 @@ public class SkillTree : MonoBehaviour {
 	public GameObject HealthPassive2;
 	public GameObject HealthPassive1;
 
-	private int HealthUpAmt;
-	private int ManaUpAmt;
+	private int PassiveUpAmt;
 	public GameObject HealthUp;
 	public GameObject ManaUp;
 	public Button HealthUp_Btn;
@@ -38,6 +40,10 @@ public class SkillTree : MonoBehaviour {
 	public Button ManaPassive2_Btn;
 	public Button ManaPassive1_Btn;
 
+	public Text healthuplevel;
+	public Text manauplevel;
+	public int healthupint;
+	public int manaupint;
 	//Weapon Tree
 	//SkillImages
 	public GameObject PowerActive1;
@@ -56,6 +62,16 @@ public class SkillTree : MonoBehaviour {
 	public Button SpeedActive2_Btn;
 	public Button SpeedPassive1_Btn;
 
+	//Class-Specific Passive
+	public bool Warborn;
+	public bool Gunslinger;
+	public bool Spellslinger;
+	public bool Spellshield;
+	public Text warborntxt;
+	public Text gunslingertxt;
+	public Text spellslingertxt;
+	public Text spellshieldtxt;
+
 	//Execute Spells/Cooldowns
 	public Image spell1;
 	public Image spell2;
@@ -71,6 +87,11 @@ public class SkillTree : MonoBehaviour {
 	public bool spell3activate;
 	public bool spell4activate;
 
+	public bool hunterbreathactivated;
+	public bool leechingbulletactivated;
+	public bool thunderrushactivated;
+	public bool berserkeractivated;
+
 	//Spell Effects prefab 
 	public GameObject skilleffect1;
 	public GameObject skilleffect2;
@@ -83,14 +104,36 @@ public class SkillTree : MonoBehaviour {
 	private GameObject Skillspelleffect3;
 	private GameObject Skillspelleffect4;
 
+	//Journal Entries
+	public Button JournalEntryBtn1;
+	public Button JournalEntryBtn2;
+	public Button JournalEntryBtn3;
+	public Button JournalEntryBtn4;
+
+	public Text JournalEntryTXT1;
+	public Text JournalEntryTXT2;
+	public Text JournalEntryTXT3;
+	public Text JournalEntryTXT4;
 
 	//Stats
-	public float incSpeed;
+	public float incSpeed; //firerate
+	public int deflectchance; //chance to deflect
+	public bool unlockdeflect;
+	public float incMSpeed; //movementrate
+	public Text WarningTxt;
+	public Text HealthStats;
+	public Text ManaStats;
+
+	public Image leechingbulletDurationImage;
+	public Image ThunderRushDurationImage;
+	public Image BerserkerDurationImage;
 	//public Toggle ChargeShot;
 	public int checkSkillPoint;
 	public Text SkillPointsTXT;
 	//public GameObject ErrorMessageTXT;
 
+	public bool classChosen = false;
+	public bool updateloopcounter = false;
 	//unlocking
 	public bool unlockHealthUp;
 	public bool unlockManaUp;
@@ -114,8 +157,10 @@ public class SkillTree : MonoBehaviour {
 	public bool disableContSkillP;
 
 	public GameObject playerobj;
-
-
+	public Transform playertransform;
+	FirstPersonController playergameobj;
+	RaycastHit hit;
+	float playercolliderradius;
 	public string slot1Skill = "";
 	public string slot2Skill = "";
 	public string slot3Skill = "";
@@ -131,8 +176,7 @@ public class SkillTree : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		checkSkillPoint = 10;
-
-
+		playercolliderradius = playerobj.GetComponent<CapsuleCollider> ().radius;
 	}
 
 
@@ -144,37 +188,139 @@ public class SkillTree : MonoBehaviour {
 			//Display SkillTree
 		}
 		SkillPointsTXT.text = checkSkillPoint.ToString ();
+		//Class-SPecific abilities Check
+		if (unlockLifePassive1 == true && unlockPowerActive1 == true && classChosen == false) {
+			Warborn = true;
+			classChosen = true;
+			gameObject.GetComponent<Health> ().maxhealth += 20;
+			gameObject.GetComponent<Health> ().healthbarslider.maxValue += 20;
+			unlockdeflect = true;
+			playerobj.GetComponent<FirstPersonController>().m_WalkSpeed -= 3.0f;
+			playerobj.GetComponent<FirstPersonController>().m_RunSpeed -= 3.0f;
+			playerobj.GetComponent<FirstPersonController>().m_JumpSpeed -= 3.0f;
+
+		} 
+		if (unlockLifePassive1 == true && unlockSpeedActive1 == true && classChosen == false) {
+			Gunslinger = true;
+			classChosen = true;
+			playerobj.GetComponent<FirstPersonController>().m_WalkSpeed += 3.5f;
+			playerobj.GetComponent<FirstPersonController>().m_RunSpeed += 3.5f;
+			playerobj.GetComponent<FirstPersonController>().m_JumpSpeed += 3.5f;
+		}  
+		if (unlockManaPassive1 == true && unlockPowerActive1 == true && classChosen == false ) {
+			classChosen = true;
+			Spellshield = true;
+			gameObject.GetComponent<Health> ().maxhealth += 5;
+			gameObject.GetComponent<Health> ().healthbarslider.maxValue += 5;
+			gameObject.GetComponent<Health> ().maxmana += 5;
+			gameObject.GetComponent<Health> ().manabarslider.maxValue += 5;
+			playerobj.GetComponent<FirstPersonController>().m_WalkSpeed -= 1.0f;
+			playerobj.GetComponent<FirstPersonController>().m_RunSpeed -= 1.0f;
+			playerobj.GetComponent<FirstPersonController>().m_JumpSpeed -= 1.0f;
+		}
+		if (unlockManaPassive1 == true && unlockSpeedActive1 == true && classChosen == false ) {
+			classChosen = true;
+			Spellslinger = true;
+			gameObject.GetComponent<Health> ().maxmana += 20;
+			gameObject.GetComponent<Health> ().manabarslider.maxValue += 20;
+			playerobj.GetComponent<FirstPersonController>().m_WalkSpeed += 1.0f;
+			playerobj.GetComponent<FirstPersonController>().m_RunSpeed += 1.0f;
+			playerobj.GetComponent<FirstPersonController>().m_JumpSpeed += 1.0f;
+
+
+		}
+
+		if (unlockManaPassive2 == true) {
+			///DO COVER ME SKILL HERE
+		}
 
 		//SPELL ABILITIES
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
 			if(slot1Skill != null){
 			switch (slot1Skill) {
 				case "Health Skill3":
-					Skillspelleffect4 = (GameObject)Instantiate (skilleffect4, playerobj.transform.position, Quaternion.identity);
-					//spell4.gameObject.transform.SetParent (playerobj.transform);
-					//Skillspelleffect4.transform.SetAsFirstSibling ();
-					spell1.gameObject.SetActive (true);
-					spell1activate = true;
+					if (gameObject.GetComponent<Health> ().manabar >= 100) {
+						Skillspelleffect4 = (GameObject)Instantiate (skilleffect4, playerobj.transform.position, Quaternion.identity);
+						//spell4.gameObject.transform.SetParent (playerobj.transform);
+						//Skillspelleffect4.transform.SetAsFirstSibling ();
+						spell1.gameObject.SetActive (true);
+						spell1activate = true;
+						hunterbreathactivated = true;
+						gameObject.GetComponent<Health> ().currentHealth += 30;
+						gameObject.GetComponent<Health> ().healthbarslider.value += 30;
+						gameObject.GetComponent<Health> ().manabar -= 100;
+						gameObject.GetComponent<Health> ().manabarslider.value -= 100;
+					} else if (spell1activate) {
+						StartCoroutine (DisplayWarning ("Ability On Cooldown", 2));
+					}
+					else {
+						StartCoroutine (DisplayWarning ("Insufficient Mana", 2));
+					}
 					break;
 				case "Mana Skill3":
-					Skillspelleffect1 = (GameObject)Instantiate (skilleffect1, playerobj.transform.position, Quaternion.identity);
-					spell1.gameObject.SetActive (true);
-					spell1activate = true;
+					if (gameObject.GetComponent<Health> ().manabar >= 35 && !spell1activate) {
+						Skillspelleffect1 = (GameObject)Instantiate (skilleffect1, playerobj.transform.position, Quaternion.identity);
+						spell1.gameObject.SetActive (true);
+						spell1activate = true;
+						leechingbulletactivated = true;
+						gameObject.GetComponent<Health> ().manabar -= 35;
+						gameObject.GetComponent<Health> ().manabarslider.value -= 35;
+						leechingbulletDurationImage.gameObject.SetActive (true);
+						StartCoroutine (SpellDurations (leechingbulletDurationImage,leechingbulletactivated, 10));
+						//StartCoroutine (SpellCoolDown (spell1,30, spell1activate));
+					} else if (spell1activate) {
+						StartCoroutine (DisplayWarning ("Ability On Cooldown", 2));
+					}
+					else {
+						StartCoroutine (DisplayWarning ("Insufficient Mana", 2));
+					}
+
 					break;
 				case "Power Skill1":
 					Skillspelleffect2 = (GameObject)Instantiate (skilleffect2, playerobj.transform.position, Quaternion.identity);
 					spell1.gameObject.SetActive (true);
 					spell1activate = true;
 					break;
-				case "Speed Skill1":
-					Skillspelleffect2 = (GameObject)Instantiate (skilleffect2, playerobj.transform.position, Quaternion.identity);
-					spell1.gameObject.SetActive (true);
-					spell1activate = true;
+				case "Speed Skill1": //Thunder Rush
+					if (gameObject.GetComponent<Health> ().manabar >= 50 && !spell1activate) {
+						Skillspelleffect2 = (GameObject)Instantiate (skilleffect2, playerobj.transform.position, Quaternion.identity);
+						spell1.gameObject.SetActive (true);
+						spell1activate = true;
+						thunderrushactivated = true;
+						gameObject.GetComponent<Health> ().manabar -= 50;
+						gameObject.GetComponent<Health> ().manabarslider.value -= 50;
+						Vector3 dashdirection = transform.forward;
+						float dashlength = 50.0f;
+						if (Physics.Raycast (transform.position, dashdirection, out hit, 50.0f + playercolliderradius)) {
+							dashlength = hit.distance - playercolliderradius;
+						}
+						playerobj.transform.position = transform.position + (dashdirection * dashlength);
+					
+						ThunderRushDurationImage.gameObject.SetActive (true);
+						StartCoroutine (SpellDurations (ThunderRushDurationImage, thunderrushactivated, 5));
+					}else if (spell1activate) {
+						StartCoroutine (DisplayWarning ("Ability On Cooldown", 2));
+					}
+					else {
+						StartCoroutine (DisplayWarning ("Insufficient Mana", 2));
+					}
 					break;
 				case "Power Skill3":
-					Skillspelleffect3 = (GameObject)Instantiate (skilleffect3, playerobj.transform.position, Quaternion.identity);
-					spell1.gameObject.SetActive (true);
-					spell1activate = true;
+					if (gameObject.GetComponent<Health> ().manabar >= 70 && !spell1activate) {
+						Skillspelleffect3 = (GameObject)Instantiate (skilleffect3, playerobj.transform.position, Quaternion.identity);
+						spell1.gameObject.SetActive (true);
+						spell1activate = true;
+						berserkeractivated = true;
+						gameObject.GetComponent<Health> ().manabar -= 70;
+						gameObject.GetComponent<Health> ().manabarslider.value -= 70;
+						BerserkerDurationImage.gameObject.SetActive (true);
+						StartCoroutine (SpellDurations (BerserkerDurationImage, berserkeractivated, 20));
+					} else if (spell1activate) {
+						StartCoroutine (DisplayWarning ("Ability On Cooldown", 2));
+					}
+					else {
+						StartCoroutine (DisplayWarning ("Insufficient Mana", 2));
+					}
 					break;
 
 				case "Speed Skill3":
@@ -305,6 +451,11 @@ public class SkillTree : MonoBehaviour {
 			}
 		
 		}
+
+		//Spells Cooldown/Time
+		if (leechingbulletactivated) {
+		}
+
 		if (spell1activate == true && spell1.fillAmount <= 0) {
 			Destroy (Skillspelleffect1);
 			spell1activate = false;
@@ -312,7 +463,11 @@ public class SkillTree : MonoBehaviour {
 			spell1.fillAmount = 1.0f;
 		}
 		if(spell1activate == true){
-			spell1.fillAmount -= 0.5f * Time.deltaTime;
+			if (berserkeractivated) {
+				spell1.fillAmount -= 1.0f / 120.0f * Time.deltaTime;
+			} else {
+				spell1.fillAmount -= 1.0f / 30.0f * Time.deltaTime;
+			}
 		}
 
 		if (spell2activate == true && spell2.fillAmount <= 0) {
@@ -347,53 +502,113 @@ public class SkillTree : MonoBehaviour {
 
 	}
 
+	IEnumerator DisplayWarning (string warning, float delay){
+		WarningTxt.text = warning;
+		WarningTxt.gameObject.SetActive (true);
+		yield return new WaitForSeconds (delay);
+		WarningTxt.gameObject.SetActive (false);
+	}
 
+	IEnumerator SpellDurations (Image spelldurationimage, bool spellactivated ,float duration){
+		yield return new WaitForSeconds (duration);
+		spellactivated = false;
+		spelldurationimage.gameObject.SetActive (false);
+	}
+
+	/*IEnumerator SpellCoolDown (Image spellobj, float cd, bool spellactivated){
+		spellobj.gameObject.SetActive (true);
+		spellobj.fillAmount = cd;
+		spellobj.fillAmount -= 1.0f * Time.deltaTime;
+		if (spellobj.fillAmount <= 0) {
+			yield return new WaitForSeconds (1);
+			spellobj.gameObject.SetActive (false);
+			spellobj.fillAmount = cd;
+			spellactivated = false;
+
+		}
+	}*/
+		
+
+	public void OnTriggerEnter(Collider obj){
+		if (obj.gameObject.name == "JournalEntry1") {
+			JournalEntryBtn1.interactable = true;
+			JournalEntryTXT1.text = "Town Of Telluris";
+			Destroy (obj.gameObject);
+		}
+		if (obj.gameObject.name == "JournalEntry2") {
+			JournalEntryBtn2.interactable = true;
+			JournalEntryTXT2.text = "Forest Of Misery";
+			Destroy (obj.gameObject);
+		}
+		if (obj.gameObject.name == "JournalEntry3") {
+			JournalEntryBtn3.interactable = true;
+			JournalEntryTXT3.text = "Temple Of Ishira";
+			Destroy (obj.gameObject);
+		}
+		if (obj.gameObject.name == "JournalEntry4") {
+			JournalEntryBtn4.interactable = true;
+			JournalEntryTXT4.text = "Crator Of Farallon";
+			Destroy (obj.gameObject);
+		}
+	}
 
 	public void SelectSkillOnClick(){
 		
 		if (checkSkillPoint > 0) {
 			if (eventsys.currentSelectedGameObject.GetComponent<Button> ().interactable == true) {
-
 				if (eventsys.currentSelectedGameObject == HealthActive1 && unlockLifeActive1 == false) {
 					checkSkillPoint -= 1;
-				} else if (eventsys.currentSelectedGameObject == ManaActive1 && unlockManaActive1 == false) {
+				}  if (eventsys.currentSelectedGameObject == ManaActive1 && unlockManaActive1 == false) {
 					checkSkillPoint -= 1;
-				} else if (eventsys.currentSelectedGameObject == PowerActive1 && unlockPowerActive1 == false) {
+				}  if (eventsys.currentSelectedGameObject == PowerActive1 && unlockPowerActive1 == false) {
 					checkSkillPoint -= 1;
-				} else if (eventsys.currentSelectedGameObject == PowerActive2 && unlockPowerActive2 == false) {
+					PowerPassive1_Btn.interactable = true;
+				}  if (eventsys.currentSelectedGameObject == PowerActive2 && unlockPowerActive2 == false) {
 					checkSkillPoint -= 1;
-				} else if (eventsys.currentSelectedGameObject == SpeedActive1 && unlockSpeedActive1 == false) {
+				}  if (eventsys.currentSelectedGameObject == SpeedActive1 && unlockSpeedActive1 == false) {
 					checkSkillPoint -= 1;
-				} else if (eventsys.currentSelectedGameObject == SpeedActive2 && unlockSpeedActive1 == false) {
+					SpeedPassive1_Btn.interactable = true;
+				}  if (eventsys.currentSelectedGameObject == SpeedActive2 && unlockSpeedActive1 == false) {
 					checkSkillPoint -= 1;
 				} else {
 					if (eventsys.currentSelectedGameObject.tag == "PassiveSkill") {
 						checkSkillPoint -= 1;
+
 					}
 				}
+
 			}
 		}
 
 	
 			if (eventsys.currentSelectedGameObject == HealthUp) {
 				unlockHealthUp = true;
-				if (HealthUpAmt <= 3) {
-					HealthUpAmt++;
+				if (PassiveUpAmt <= 6) {
+				PassiveUpAmt++;
+				healthupint++;
+				healthuplevel.text = "Level " + healthupint.ToString();
 				gameObject.GetComponent<Health> ().maxhealth += 5;
 				gameObject.GetComponent<Health> ().healthbarslider.maxValue += 5;
 				} else {
+
 					HealthUp_Btn.interactable = false;
+					ManaUp_Btn.interactable = false;
+					
+
 				}
 
 			}
 			else if(eventsys.currentSelectedGameObject == ManaUp){
 				unlockManaUp = true;
-				if (ManaUpAmt <= 3) {
-					ManaUpAmt++;
+			if (PassiveUpAmt <= 6) {
+				PassiveUpAmt++;
+				manaupint++;
+				manauplevel.text = "Level " + manaupint.ToString();
 				gameObject.GetComponent<Health> ().maxmana += 5;
 				gameObject.GetComponent<Health> ().manabarslider.maxValue += 5;
 				} else {
-					ManaUp_Btn.interactable = false;
+				HealthUp_Btn.interactable = false;
+				ManaUp_Btn.interactable = false;
 				}
 
 			}
@@ -404,6 +619,7 @@ public class SkillTree : MonoBehaviour {
 			}
 		else if (eventsys.currentSelectedGameObject == HealthPassive2) {
 				unlockLifePassive2 = true;
+				
 
 			}
 		else if (eventsys.currentSelectedGameObject == HealthActive1) {
@@ -428,7 +644,7 @@ public class SkillTree : MonoBehaviour {
 			
 			}
 		else if (eventsys.currentSelectedGameObject == PowerPassive1) {
-			
+			unlockPowerPassive1 = true;
 		
 			}
 		else if (eventsys.currentSelectedGameObject == PowerActive2) {
@@ -455,6 +671,23 @@ public class SkillTree : MonoBehaviour {
 		SkillPointsTXT.text = checkSkillPoint.ToString ();
 	}
 		
+	public void playerstatstab(){
+		HealthStats.text = gameObject.GetComponent<Health> ().maxhealth.ToString () + " %";
+		ManaStats.text = gameObject.GetComponent<Health> ().maxmana.ToString () + " %";
+		if (Warborn) {
+			warborntxt.gameObject.SetActive (true);
+		}
+		if (Gunslinger) {
+			gunslingertxt.gameObject.SetActive (true);
+		}
+		if (Spellshield) {
+			spellshieldtxt.gameObject.SetActive (true);
+		}
+		if (Spellslinger) {
+			spellslingertxt.gameObject.SetActive (true);
+		}
+	}
+
 
 	void updateMagicPoints(){
 		//SkillPointsTXT.text = "" + GameObject.Find("Player").GetComponent<PlayerLevelSystem>().MagicSkillPoints;
--- Mimori FYP Game/Assets/Scripts/Player/BulletController.cs
+++ Mimori FYP Game/Assets/Scripts/Player/BulletController.cs
@@ -2,8 +2,18 @@
 using System.Collections;
 
 public class BulletController : MonoBehaviour {
+    public GameObject bulletImpact;
+	public double playerbasedamage = 10;
+	public double playercurrdamage;
+
+	public float playertakendamage = 2.5f;
+	public float playercurrtakendamage;
     private GameObject player;
+	//public SkillTree sktree;
 	private float liveTime = 1f;
+	public int fullychargedbullet = 2;
+	public bool fullychargedbulletactivated;
+	public bool counterupdateloop = false;
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -18,44 +28,116 @@ public class BulletController : MonoBehaviour {
 		if (liveTime <= 0) {
 			Destroy (this.gameObject);
 		}
-	}
+		if (player.GetComponent<SkillTree> ().unlockLifePassive1 && player.GetComponent<Health> ().currentHealth < player.GetComponent<Health> ().maxhealth / 2) {
+			playercurrdamage = playerbasedamage / 100 * 120;
+		} else {
+			playercurrdamage = playerbasedamage;
+		}
+		if (player.GetComponent<SkillTree> ().unlockManaPassive1 && player.GetComponent<Health> ().manabar >= player.GetComponent<Health> ().maxmana) {
+			if (fullychargedbullet > 0 && fullychargedbullet <= 2) {
+				fullychargedbulletactivated = true;
+				playercurrdamage = playerbasedamage * 2;	
+			}
+		} else {
+			if (playercurrdamage == playerbasedamage * 2) {
+				playercurrdamage = playerbasedamage;
+				fullychargedbulletactivated = false;
+				fullychargedbullet = 2;
+			}
+		}
+		if (player.GetComponent<SkillTree> ().unlockPowerPassive1) {
+			playerbasedamage = 25;
+		}
+		if (player.GetComponent<SkillTree> ().thunderrushactivated) {
+			playercurrdamage = playerbasedamage + 5;
+		} else {
+			playercurrdamage = playerbasedamage;
+		}
 
+		if (player.GetComponent<SkillTree> ().berserkeractivated) {
+			playercurrdamage = playerbasedamage + 15;
+			playercurrtakendamage = playertakendamage + 15;
+		} else {
+			playercurrdamage = playerbasedamage;
+			playercurrtakendamage = playertakendamage;
+		}
+	}
 	void OnTriggerEnter(Collider other){
         if(Vector3.Distance(other.transform.position, player.transform.position) <= 40)
         {
-            if (other.gameObject.tag == "MeleeMinion")
+            if (this.gameObject.tag != "EnemySkill")
             {
-                other.GetComponent<MeleeMinionFSM>().getHit(10);
-                Destroy(this.gameObject);
-                Player.inCombatCD = 10;
-            }
-            if (other.gameObject.tag == "RangeMinion")
-            {
-                other.GetComponent<RangeMinionFSM>().getHit(10);
-                Destroy(this.gameObject);
-                Player.inCombatCD = 10;
-            }
+                if (other.gameObject.tag == "MeleeMinion")
+                {
+                    other.GetComponent<MeleeMinionFSM>().getHit(playercurrdamage);
+					if (player.GetComponent<SkillTree>().leechingbulletactivated) {
+						player.gameObject.GetComponent<Health> ().manabar += 10;
+						player.gameObject.GetComponent<Health> ().manabarslider.value += 10;
+					}
+					if (fullychargedbulletactivated) {
+						fullychargedbullet--;
+					}
+                    DestroyBullet();
+                }
+                else if (other.gameObject.tag == "RangeMinion")
+                {
+					other.GetComponent<RangeMinionFSM>().getHit(playercurrdamage);
+					if (player.GetComponent<SkillTree>().leechingbulletactivated) {
+						player.gameObject.GetComponent<Health> ().manabar += 10;
+						player.gameObject.GetComponent<Health> ().manabarslider.value += 10;
+					}
+					if (fullychargedbulletactivated) {
+						fullychargedbullet--;
+					}
+                    DestroyBullet();
+                }
 
-			if (other.gameObject.tag == "HordeMeleeMinion")
-			{
-				other.GetComponent<HordeMeleeMinion>().getHit(10);
-				Destroy(this.gameObject);
-                Player.inCombatCD = 10;
-            }
-			if (other.gameObject.tag == "HordeRangeMinion")
-			{
-				other.GetComponent<HordeRangeMinion>().getHit(10);
-				Destroy(this.gameObject);
-                Player.inCombatCD = 10;
+                else if (other.gameObject.tag == "HordeMeleeMinion")
+                {
+					other.GetComponent<HordeMeleeMinion>().getHit(playercurrdamage);
+					if (player.GetComponent<SkillTree>().leechingbulletactivated) {
+						player.gameObject.GetComponent<Health> ().manabar += 10;
+						player.gameObject.GetComponent<Health> ().manabarslider.value += 10;
+					}
+					if (fullychargedbulletactivated) {
+						fullychargedbullet--;
+					}
+                    DestroyBullet();
+                }
+                else if (other.gameObject.tag == "HordeRangeMinion")
+                {
+					other.GetComponent<HordeRangeMinion>().getHit(playercurrdamage);
+					if (player.GetComponent<SkillTree>().leechingbulletactivated) {
+						player.gameObject.GetComponent<Health> ().manabar += 10;
+						player.gameObject.GetComponent<Health> ().manabarslider.value += 10;
+					}
+					if (fullychargedbulletactivated) {
+						fullychargedbullet--;
+					}
+                    DestroyBullet();
+                }
+				else if(other.gameObject.tag == "InvisibleWall" && other.gameObject.tag == "DialogueTrigger"){
+                    GameObject bullet = (GameObject)Instantiate(bulletImpact, this.transform.position, Quaternion.identity);
+					if (fullychargedbulletactivated) {
+						fullychargedbullet--;
+					}
+					Destroy(this.gameObject);
+                }
             }
         }
 
         if (this.gameObject.tag == "EnemySkill") {
             if (other.gameObject.tag == "Player") {
-                other.GetComponent<Health>().currentHealth -= 2.5f;
+                other.GetComponent<Health>().currentHealth -= playercurrtakendamage;
 				Destroy (this.gameObject);
             }
         }
 		
 	}
+
+    void DestroyBullet() {
+        GameObject bullet = (GameObject)Instantiate(bulletImpact, this.transform.position, Quaternion.identity);
+        Player.inCombatCD = 10;
+        Destroy(this.gameObject);
+    }
 }
