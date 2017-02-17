e81fdccf2f08a5dbcfef7f50b1f20edfa2f53169
Auraby <tan_alwin97@yahoo.com.sg>
2017-02-17 15:46:49 +0800

    Commit to fix meta file missing prefab problem
    -Added credits scene also

---
+++ Mimori FYP Game/Assets/Scripts/CreditsSceneManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CreditsSceneManager : MonoBehaviour {
+
+    public GameObject thankyouText;
+
+    public float timeTillThankYouAppear;
+    private float tempCreditsTime;
+
+    AsyncOperation asyncOp;
+
+	// Use this for initialization
+	void Start () {
+        tempCreditsTime = 0;
+        thankyouText.SetActive(false);
+
+        asyncOp = SceneManager.LoadSceneAsync("MainMenu");
+        asyncOp.allowSceneActivation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempCreditsTime += Time.deltaTime;
+
+        if(tempCreditsTime > timeTillThankYouAppear)
+        {
+            thankyouText.SetActive(true);
+        }
+	}
+
+    public void crdReturnToMenuBtnPressed()
+    {
+        asyncOp.allowSceneActivation = true;
+    }
+}
